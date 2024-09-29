188f5f8f8670dfe31accc8a22f58f4c22b69b0c0
周骞 <102906573+Zqiancreator@users.noreply.github.com>
2024-09-29 13:53:26 +0800

    添加翻译和修复func加了block后没有下拉框

--- DFRobot_S12SD.ts
+++ DFRobot_S12SD.ts
@@ -1,11 +1,3 @@
-enum dataType {
-    //% block="Value"
-    Value,
-    //% block="Index"
-    Index,
-    //% block="RiskLevel"
-    RiskLevel,
-}
 let I2CAddr = 0x23                   ///<I2C address
 let S12SD_INPUTREG_UVS_DATA = 0x06   ///<UVS data
 let S12SD_INPUTREG_UVS_INDEX = 0x07   ///<UVS index
@@ -15,26 +7,34 @@ let S12SD_INPUTREG_RISK_LEVEL = 0x08   ///<RISK LEVEL
  */
 //% weight=100 color=#00F5FF icon="\uf185" block="DFRobot_S12SD"
 namespace DFRobot_S12SD {
+    export enum eDataType {
+        //% block="Value"
+        Value,
+        //% block="Index"
+        Index,
+        //% block="RiskLevel"
+        RiskLevel,
+    }
     /**
      * Read ultraviolet data
      * @param eType describe Data Type
      */
-    //% block
-    export function readUv(eType: dataType): number {
+    //% block="read UV %eType"
+    export function readUv(eType: eDataType): number {
         let readBuffer
         let ret = 0
         switch (eType) {
-            case dataType.RiskLevel:
+            case eDataType.RiskLevel:
                 pins.i2cWriteNumber(I2CAddr, S12SD_INPUTREG_RISK_LEVEL, NumberFormat.Int8LE)
                 readBuffer = pins.i2cReadBuffer(I2CAddr, 2)
                 ret = ((readBuffer[1] << 8) | readBuffer[0]) & 0xFFFF
                 break
-            case dataType.Value:
+            case eDataType.Value:
                 pins.i2cWriteNumber(I2CAddr, S12SD_INPUTREG_UVS_DATA, NumberFormat.Int8LE)
                 readBuffer = pins.i2cReadBuffer(I2CAddr, 2)
                 ret = ((readBuffer[1] << 8) | readBuffer[0]) & 0xFFFF
                 break
-            case dataType.Index:
+            case eDataType.Index:
                 pins.i2cWriteNumber(I2CAddr, S12SD_INPUTREG_UVS_INDEX, NumberFormat.Int8LE)
                 readBuffer = pins.i2cReadBuffer(I2CAddr, 2)
                 ret = ((readBuffer[1] << 8) | readBuffer[0]) & 0xFFFF
--- main.ts
+++ main.ts
@@ -1,16 +1,3 @@
-let index = 0
-let level = 0
-let value = 0
 basic.forever(function () {
-    index = DFRobot_S12SD.readUv(dataType.Index)
-    serial.writeString("index ")
-    serial.writeLine("" + (index))
-    basic.pause(100)
-    level = DFRobot_S12SD.readUv(dataType.RiskLevel)
-    serial.writeString("level")
-    serial.writeLine("" + (level))
-    basic.pause(100)
-    value = DFRobot_S12SD.readUv(dataType.Value)
-    serial.writeString("value")
-    serial.writeLine("" + (value))
+	
 })
